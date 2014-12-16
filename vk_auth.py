9833a9900bac47a1804ff1783e9c7b7c8d35f6a1
LexPanov <panov1552@gmail.com>
2014-12-16 18:41:59 +0300

    vk_auth beautify

--- vk_auth.py
+++ vk_auth.py
@@ -12,6 +12,7 @@ class FormParser(HTMLParser):
         self.in_form = False
         self.form_parsed = False
         self.method = "GET"
+
     def handle_starttag(self, tag, attrs):
         tag = tag.lower()
         if tag == "form":
@@ -30,6 +31,7 @@ class FormParser(HTMLParser):
         elif tag == "input" and "type" in attrs and "name" in attrs:
             if attrs["type"] in ["hidden", "text", "password"]:
                 self.params[attrs["name"]] = attrs["value"] if "value" in attrs else ""
+
     def handle_endtag(self, tag):
         tag = tag.lower()
         if tag == "form":
@@ -39,8 +41,11 @@ class FormParser(HTMLParser):
             self.form_parsed = True
 
 def auth_user(email, password, client_id, scope, opener):
-    response = opener.open("http://oauth.vk.com/oauth/authorize?" + \
-        "redirect_uri=http://oauth.vk.com/blank.html&response_type=token&client_id=%s&scope=%s&display=wap" % (client_id, ",".join(scope)))
+    response = opener.open(
+        "http://oauth.vk.com/oauth/authorize?" + \
+        "redirect_uri=http://oauth.vk.com/blank.html&response_type=token&" + \
+        "client_id=%s&scope=%s&display=wap" % (client_id, ",".join(scope))
+        )
     doc = response.read()
     parser = FormParser()
     parser.feed(doc.decode('utf-8'))
@@ -50,17 +55,24 @@ def auth_user(email, password, client_id, scope, opener):
           raise RuntimeError("Something wrong")
     parser.params["email"] = email
     parser.params["pass"] = password
-    if parser.method.upper() == "POST":response = opener.open(parser.url, urllib.parse.urlencode(parser.params).encode('utf-8'))
-    else:raise NotImplementedError("Method '%s'" % parser.method)
+    if parser.method.upper() == "POST":
+        response = opener.open(parser.url, urllib.parse.urlencode(parser.params).encode('utf-8'))
+    else:
+        raise NotImplementedError("Method '%s'" % parser.method)
     return response.read(), response.geturl()
+
 def give_access(doc, opener):
     parser = FormParser()
     parser.feed(doc.decode('utf-8'))
     parser.close()
-    if not parser.form_parsed or parser.url is None:raise RuntimeError("Something wrong")
-    if parser.method.upper() == "POST":response = opener.open(parser.url, urllib.parse.urlencode(parser.params).encode('utf-8'))
-    else:raise NotImplementedError("Method '%s'" % params.method)
+    if not parser.form_parsed or parser.url is None:
+          raise RuntimeError("Something wrong")
+    if parser.method.upper() == "POST":
+        response = opener.open(parser.url, urllib.parse.urlencode(parser.params).encode('utf-8'))
+    else:
+        raise NotImplementedError("Method '%s'" % params.method)
     return response.geturl()
+
 
 def auth(email, password, client_id, scope):
     if not isinstance(scope, list):
@@ -69,13 +81,18 @@ def auth(email, password, client_id, scope):
         urllib.request.HTTPCookieProcessor(http.cookiejar.CookieJar()),
         urllib.request.HTTPRedirectHandler())
     doc, url = auth_user(email, password, client_id, scope, opener)
-    if urlparse(url).path != "/blank.html":url = give_access(doc, opener)# Need to give access to requested scope
-    if urlparse(url).path != "/blank.html":raise RuntimeError("Expected success here")
-    
+    if urlparse(url).path != "/blank.html":
+        # Need to give access to requested scope
+        url = give_access(doc, opener)
+    if urlparse(url).path != "/blank.html":
+        raise RuntimeError("Expected success here")
+
     def split_key_value(kv_pair):
         kv = kv_pair.split("=")
         return kv[0], kv[1]
+
     answer = dict(split_key_value(kv_pair) for kv_pair in urlparse(url).fragment.split("&"))
     if "access_token" not in answer or "user_id" not in answer:
         raise RuntimeError("Missing some values in answer")
     return answer["access_token"], answer["user_id"] 
+
